129669f151cad0a5a83bf3a3718427dd88ea3847
Son Do <dominhson580@gmail.com>
2024-02-18 10:41:54 +0700

    Re-organize code

---
+++ 1-Introduction/challenges/java/Helper.java
@@ -0,0 +1,5 @@
+public class Helper {
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+}
---
+++ 1-Introduction/challenges/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Helper helper = new Helper();
+        String message = "Hello, World!";
+        helper.printMessage(message);
+    }
+}
