ed07258d82ba1668a6b7396edc9f84fb39ae4735
Son Do <dominhson580@gmail.com>
2026-05-09 15:40:15 +0700

    clox: scanner

--- clox/vm.c
+++ clox/vm.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "vm.h"
 
-VM vm; // only need 1 VM instance
+VM vm;
 
 static void resetStack()
 {
@@ -102,9 +103,8 @@ static InterpretResult run()
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk *chunk)
+InterpretResult interpret(const char *source)
 {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+    compile(source);
+    return INTERPRET_OK;
 }
--- clox/vm.h
+++ clox/vm.h
@@ -23,8 +23,8 @@ typedef enum
 
 void initVM();
 void freeVM();
-InterpretResult interpret(Chunk *chunk);
+InterpretResult interpret(const char *source);
 void push(Value value);
-Value pop(); 
+Value pop();
 
 #endif
